0b3083bf53564a077cd0b28ed50847dfe4f8e9eb
Junior Reis <33906887+reisjunior1@users.noreply.github.com>
2023-07-08 15:02:04 -0300

    Corrigir validacao para exibir mensagem de erro

--- app/Http/Controllers/CadastroJagadoresController.php
+++ app/Http/Controllers/CadastroJagadoresController.php
@@ -84,7 +84,7 @@ class CadastroJagadoresController extends Controller
 
     public function pesquisar(Request $request, $pesquisar = null, $recuperaDados = null)
     {
-        if (empty($request->mlJogador) && $pesquisar == null && $recuperaDados == null) {
+        if (empty($request->mlJogador) && is_null($recuperaDados)) {
             session()->flash(
                 'mensagem',
                 "Selecione pelo menos um Jogador!"
